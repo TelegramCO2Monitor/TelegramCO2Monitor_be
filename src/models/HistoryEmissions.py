473d3f8e412db884cdc8229c7e6883182bbafd23
Francesco Ricci <francescosav.ricci@libero.it>
2024-06-10 16:48:12 +0200

    fix: correct style

--- src/models/HistoryEmissions.py
+++ src/models/HistoryEmissions.py
@@ -1,6 +1,6 @@
 class HistoryEmissions:
 
-    def __init__(self, timestamp , emission_day , type_history):
+    def __init__(self, timestamp, emission_day, type_history):
         self.timestamp = timestamp
         self.emission_day = emission_day
         self.type_history = type_history
--- src/models/Message.py
+++ src/models/Message.py
@@ -1,7 +1,7 @@
 class Message:
 
-    def __init__(self,type_message,size,timestamp,user_id):
+    def __init__(self, type_message, size, timestamp, user_id):
         self.type_message = type_message
         self.size = size
-        self.timestamp  = timestamp
+        self.timestamp = timestamp
         self.user_id = user_id
