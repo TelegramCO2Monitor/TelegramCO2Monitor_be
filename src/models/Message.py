b436492f9a8117c275e1671ea3a3abd636d475cb
Francesco Ricci <francescosav.ricci@libero.it>
2024-06-10 15:42:53 +0200

    feat: add class of data model

---
+++ src/models/Message.py
@@ -0,0 +1,7 @@
+class Message:
+
+    def __init__(self,type_message,size,timestamp,user_id):
+        self.type_message = type_message
+        self.size = size
+        self.timestamp  = timestamp
+        self.user_id = user_id
