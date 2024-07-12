f574a6996761146654d9ecce24c5a4a7275f8cae
Lara Filippone <lara.filippone@gmail.com>
2024-07-12 18:47:50 +0200

    fix: make bot and app async

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from src.app.api.api import router as api_router
 from src.app.depends import init_db, get_telegram_bot
 import asyncio
@@ -6,12 +7,25 @@ import uvicorn
 
 app = FastAPI()
 
+telegram_bot = None
 
-@app.on_event("startup")
-async def startup_event():
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global telegram_bot
     init_db()
+
     telegram_bot = get_telegram_bot()
-    asyncio.create_task(telegram_bot.application.start())
+    await telegram_bot.initialize()
+    task = asyncio.create_task(telegram_bot.start())
+
+    yield
+
+    await telegram_bot.application.stop()
+    task.cancel()
+
+
+app.router.lifespan_context = lifespan
 
 app.include_router(api_router, prefix="/api")
 
--- src/repositories/TelegramBot.py
+++ src/repositories/TelegramBot.py
@@ -1,6 +1,5 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
-
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 class TelegramBot:
     def __init__(self, token: str):
@@ -8,25 +7,39 @@ class TelegramBot:
         self.application = Application.builder().token(self.token).build()
         self.application.add_handler(CommandHandler("start", self.start))
         self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_message_text))
+        self.application.add_handler(CommandHandler("info", self.info))
 
     @staticmethod
     async def start(update: Update) -> None:
         await update.message.reply_text(f"Ciao dal nostro bellissimo bot, {update.message.from_user.first_name}")
 
     @staticmethod
+    async def info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                     InlineKeyboardButton("Option 2", callback_data='2')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+    @staticmethod
     def co2_text_message_calculator(message):
         co2_emissions_per_byte = 2.1
         return len(message) * co2_emissions_per_byte
 
-    async def handle_message_text(self, update: Update) -> None:
+    async def handle_message_text(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         total_emissions = 0
         message = update.message.text
         total_emissions += self.co2_text_message_calculator(message)
-
         metadata_emissions = self.co2_text_message_calculator(update.message.to_json())
-
         total_emissions += metadata_emissions
-        message = (f'Your only message emits {total_emissions}ng of Co2 for group member\n\nFull metadata message emits'
-                   f' {metadata_emissions}ng of Co2 for group member')
+        reply_message = (f'Your message emits {total_emissions}ng of CO2\nFull metadata emits {metadata_emissions}ng of CO2')
+        await update.message.reply_text(reply_message)
 
-        await update.message.reply_text(message)
+    async def initialize(self):
+        await self.application.initialize()
+
+    async def start(self):
+        await self.application.start()
+        await self.application.updater.start_polling()
+
+    async def stop(self):
+        await self.application.stop()
